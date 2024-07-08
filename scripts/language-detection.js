c52024c2f0bae9214a603dbbd42e31923b3ec161
Vermoot <vermoot@gmail.com>
2024-07-08 22:57:40 +0200

    Ajout d'un bouton pour override la langue

--- scripts/language-detection.js
+++ scripts/language-detection.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     const userLang = navigator.language || navigator.userLanguage;
+    const storedLang = localStorage.getItem('preferredLanguage');
 
     const englishElements = document.querySelectorAll('.en');
     const frenchElements = document.querySelectorAll('.fr');
 
-    if (userLang.startsWith('fr')) {
-        englishElements.forEach(element => {
-            element.style.display = 'none';
-        });
-    } else {
-        frenchElements.forEach(element => {
-            element.style.display = 'none';
-        });
+    function toggleLanguage(lang) {
+        if (lang === 'fr') {
+            englishElements.forEach(element => {
+                element.style.display = 'none';
+            });
+        } else {
+            frenchElements.forEach(element => {
+                element.style.display = 'none';
+            });
+        }
     }
+
+    // Use stored language preference if it exists, otherwise use browser language
+    const preferredLanguage = storedLang || (userLang.startsWith('fr') ? 'fr' : 'en');
+    toggleLanguage(preferredLanguage);
+
+    // Add event listeners to buttons
+    document.getElementById('btn-en').addEventListener('click', function() {
+        localStorage.setItem('preferredLanguage', 'en');
+        location.reload();
+    });
+
+    document.getElementById('btn-fr').addEventListener('click', function() {
+        localStorage.setItem('preferredLanguage', 'fr');
+        location.reload();
+    });
 });
