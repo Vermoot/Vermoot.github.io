2b4661d206e8173b3cce60ffa26e060d2663bb9b
Vermoot <vermoot@gmail.com>
2024-11-12 16:45:43 +0100

    about page and commont header and footer

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Define the header content
+  const footerContent = `
+      <footer class="card">
+        <p>&copy; 2024 Vermoot</p>
+      </footer>
+  `;
+
+  // Insert the header into the placeholder
+  document.getElementById("footer").innerHTML = footerContent;
+
+});
---
+++ js/header.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Define the header content
+  const headerContent = `
+      <header class="card">
+        <div class="logo">
+          <a href="index.html"><img src="assets/images/logo.png" alt="Logo"></a>
+        </div>
+        <nav>
+          <div class="fr nav">
+            <a href="#details">Détails</a>
+            <a href="about.html">À propos</a>
+            <a href="#contact">Contact</a>
+            <button id="btn-en"></button>
+          </div>
+          <div class="en nav">
+            <a href="#details">Details</a>
+            <a href="about.html">About me</a>
+            <a href="#contact">Contact</a>
+            <button id="btn-fr"></button>
+          </div>
+        </nav>
+      </header>
+  `;
+
+  // Insert the header into the placeholder
+  document.getElementById("header").innerHTML = headerContent;
+
+});
